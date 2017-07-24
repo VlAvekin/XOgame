fc050ec05ca941ab23ee47be10b4c932f864dc5b
VlAvekin <vladaavekin@mail.com>
2017-07-25 02:12:14 +0300

    Winner Controller update

--- src/com/gmail/vladaavekin/Сontroller/WinnerController.java
+++ src/com/gmail/vladaavekin/Сontroller/WinnerController.java
@@ -17,20 +17,20 @@ public class WinnerController {
             for (int i = 0; i < 3; i++) {
 
                 if (chec(fields, new Point(i, 0), new Point(i, 1), new Point(i, 2)))
-                    return fields.getFields(new Point(i, 0));
+                    return fields.getFigure(new Point(i, 0));
 
             }
 
             for (int i = 0; i < 3; i++) {
 
                 if (chec(fields, new Point(0, i), new Point(1, i), new Point(2, i)))
-                    return fields.getFields(new Point(0, i));
+                    return fields.getFigure(new Point(0, i));
 
                 if (chec(fields, new Point(0, 0), new Point(1, 1), new Point(2, 2)))
-                    return fields.getFields(new Point(0, 0));
+                    return fields.getFigure(new Point(0, 0));
 
                 if (chec(fields, new Point(2, 0), new Point(1, 1), new Point(0, 2)))
-                    return fields.getFields(new Point(1, 1));
+                    return fields.getFigure(new Point(1, 1));
 
             }
 
@@ -47,10 +47,10 @@ public class WinnerController {
 
         try {
 
-            if (fields.getFields(p1) == null) return false;
+            if (fields.getFigure(p1) == null) return false;
 
-            if (fields.getFields(p2) == fields.getFields(p2) &&
-                    fields.getFields(p1) == fields.getFields(p3))
+            if (fields.getFigure(p2) == fields.getFigure(p2) &&
+                    fields.getFigure(p1) == fields.getFigure(p3))
                 return true;
 
         } catch (InvalidPointException e) {
