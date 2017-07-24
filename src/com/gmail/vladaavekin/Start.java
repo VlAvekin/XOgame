ad13f15837a7dc667216e6c8477e416caccb6ba3
VlAvekin <vladaavekin@mail.com>
2017-07-24 22:04:18 +0300

    View Console

--- src/com/gmail/vladaavekin/Start.java
+++ src/com/gmail/vladaavekin/Start.java
@@ -1,9 +1,30 @@
 package com.gmail.vladaavekin;
 
 
+import com.gmail.vladaavekin.Model.Fields;
+import com.gmail.vladaavekin.Model.Figure;
+import com.gmail.vladaavekin.Model.Game;
+import com.gmail.vladaavekin.Model.Player;
+import com.gmail.vladaavekin.View.Console.ViewConsole;
+
 public class Start {
 
     public static void main(String[] args) {
+
+        String GameName = "XO-game";
+
+        String name[] = new  String[2];
+        name[0] = "Vlad";
+        name[1] = "NoNeme";
+
+        final Player[] players = new Player[2];
+        players[0] = new Player(name[0], Figure.X);
+        players[1] = new Player(name[1], Figure.Y);
+
+        Game game = new Game(GameName, players, new Fields(3,3));
+
+        final ViewConsole viewConsole = new ViewConsole();
+        viewConsole.show(game);
 
 
 
---
+++ src/com/gmail/vladaavekin/View/Console/ViewConsole.java
@@ -0,0 +1,64 @@
+package com.gmail.vladaavekin.View.Console;
+
+
+import com.gmail.vladaavekin.Model.Fields;
+import com.gmail.vladaavekin.Model.Game;
+
+import java.util.Scanner;
+
+public class ViewConsole {
+
+
+    public void show(Game game) {
+
+        System.out.println("Game name: " + game.getNameGame());
+        printField(game);
+
+    }
+
+    public static void printField(Game game) {
+
+        final Fields fields = game.getFields();
+
+        for (int i = 0; i < fields.getCoordinateX(); i++) {
+
+            printFor(fields, i);
+
+            if (i <= fields.getCoordinateX() - 2) {
+                printSeparator();
+            }
+
+        }
+
+    }
+
+    public int ascCoordinat() {
+
+        System.out.print("Input : ");
+        Scanner scanner = new Scanner(System.in);
+        int in = scanner.nextInt();
+        return in;
+
+    }
+
+    public static void printFor(final Fields fields, final int x) {
+
+        for (int i = 0; i < fields.getCoordinateY(); i++) {
+
+            System.out.print(fields.getFields()[x][i] != null ? fields.getFields()[x][i] : " ");
+
+            if (i < fields.getCoordinateY() - 1) {
+                System.out.print(" | ");
+            }
+
+        }
+        System.out.println();
+    }
+
+    public static void printSeparator(){
+
+        System.out.print("~~~~~~~~~\n");
+
+    }
+
+}
