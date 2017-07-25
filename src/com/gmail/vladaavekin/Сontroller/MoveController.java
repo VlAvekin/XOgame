56f5be1415eb76afef7f8538fee65031b8426d62
VlAvekin <vladaavekin@mail.com>
2017-07-25 03:04:40 +0300

    Move Controller update

--- src/com/gmail/vladaavekin/Сontroller/MoveController.java
+++ src/com/gmail/vladaavekin/Сontroller/MoveController.java
@@ -1,7 +1,25 @@
 package com.gmail.vladaavekin.Сontroller;
 
+import com.gmail.vladaavekin.Model.Fields;
+import com.gmail.vladaavekin.Model.Figure;
+import com.gmail.vladaavekin.Model.exceptions.AlreadyOccupiedException;
+import com.gmail.vladaavekin.Model.exceptions.InvalidPointException;
+
+import java.awt.*;
+
 public class MoveController {
 
+    public void applyFigure(final Fields fields,
+                            final Point point,
+                            final Figure figure) throws InvalidPointException, AlreadyOccupiedException {
 
+        if (fields.getFigure(point) != null) {
+
+            throw new AlreadyOccupiedException();
+
+        }
+
+        fields.setFields(point, figure);
+    }
 
 }
